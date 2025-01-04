9a07f55bbd37c725a1d1b1d4b18b5ed5f1575732
raistmere <raistmere@gmail.com>
2025-01-04 11:56:12 -0600

    feat: added spring boot starter project

---
+++ src/main/java/com/raistmere/notetakingwebapp/NoteTakingWebappApplication.java
@@ -0,0 +1,13 @@
+package com.raistmere.notetakingwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteTakingWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteTakingWebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raistmere/notetakingwebapp/NoteTakingWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raistmere.notetakingwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteTakingWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
